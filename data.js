e0669a8c3b106c11dc23c81c0d1a4a8c852d88f2
mohammad-rahi <mohammadrahi003@gmail.com>
2022-08-04 18:06:15 +0600

    first commit

---
+++ data.js
@@ -0,0 +1,32 @@
+const courses = [
+    {
+        id: 1,
+        img: "./assets/img/t-1.png",
+        title: "Grapic design course",
+    },
+    {
+        id: 2,
+        img: "./assets/img/t-2.png",
+        title: "Grapic design course",
+    },
+    {
+        id: 3,
+        img: "./assets/img/t-3.png",
+        title: "Grapic design course",
+    },
+    {
+        id: 4,
+        img: "./assets/img/t-4.png",
+        title: "Grapic design course",
+    },
+    {
+        id: 5,
+        img: "./assets/img/t-3.png",
+        title: "Grapic design course",
+    },
+    {
+        id: 6,
+        img: "./assets/img/t-4.png",
+        title: "Grapic design course",
+    },
+];
